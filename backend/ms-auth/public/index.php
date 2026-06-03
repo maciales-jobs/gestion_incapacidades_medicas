4aac8c4db9d37cbc6fef8d2925f6c2c52e432e17
Jeisson Marciales <jdmarciales@uniboyaca.edu.co>
2026-06-03 18:25:50 -0500

    Jeisson: Mejora CORS con Allow-Credentials y manejo de OPTIONS

--- backend/ms-auth/public/index.php
+++ backend/ms-auth/public/index.php
@@ -11,10 +11,18 @@ $app->addBodyParsingMiddleware();
 
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
-    return $response
+    
+    $response = $response
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->withHeader('Access-Control-Allow-Credentials', 'true');
+    
+    if ($request->getMethod() === 'OPTIONS') {
+        return $response->withStatus(200);
+    }
+    
+    return $response;
 });
 
 $routes = require __DIR__ . '/../app/Routes/routes.php';
